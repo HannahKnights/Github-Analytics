7eb2cbce88da1d1dc61790edb6b8d20d37216a9e
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-02 13:55:09 +0000

    Authenticating client for Octokit Github login with OAuth

--- lib/githubanalytics.rb
+++ lib/githubanalytics.rb
@@ -1,1 +1,10 @@
-githubanalytics.rb
+require 'octokit'
+
+# Setting Octokit up with O-Auth authentication
+
+def client
+  client = Octokit::Client.new :access_token => ENV['GITHUB_OAUTH']
+  client.user.login
+end
+
+
---
+++ spec/github_spec.rb
@@ -0,0 +1,10 @@
+require 'githubanalytics'
+
+
+describe 'setting up a client' do
+  
+  it 'should have an authenticated client' do
+    expect(client).to eq 'HannahKnights'
+  end
+
+end
